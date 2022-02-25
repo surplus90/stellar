87af8c365679151d3485a326d7b4268bff368f08
surplus90 <surplus90@gmail.com>
2022-02-25 20:48:59 +0900

    response 모두 ResponseEntity 으로 변경

--- src/main/java/com/ponyz/stellar/web/api/FortuneTellingController.java
+++ src/main/java/com/ponyz/stellar/web/api/FortuneTellingController.java
@@ -8,14 +8,12 @@ import com.ponyz.stellar.web.vo.ReservationDetailVo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/fortune-telling")
@@ -26,9 +24,9 @@ public class FortuneTellingController {
     private final ReservationRepository reservationRepository;
 
     @GetMapping("/reservations")
-    public List<Reservation> getReservations() {
+    public ResponseEntity<List<Reservation>> getReservations() {
         List<Reservation> data = reservationRepository.findAllByOrderByCreatedAtDesc();
-        return data;
+        return ResponseEntity.ok().body(data);
     }
 
     @GetMapping("/reservations/{idx}")
@@ -48,7 +46,7 @@ public class FortuneTellingController {
     }
 
     @PostMapping("/setting")
-    public String addSetting(@RequestBody SettingToSpreadingDto settingToSpreadingDto) {
+    public ResponseEntity addSetting(@RequestBody SettingToSpreadingDto settingToSpreadingDto) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
         LocalDateTime reservationAt = LocalDateTime.parse(settingToSpreadingDto.getReservationAt(), formatter);
 
@@ -58,11 +56,11 @@ public class FortuneTellingController {
                 .selectedCards(settingToSpreadingDto.getSelectedCards())
                 .reservationAt(reservationAt)
                 .build());
-        return settingToSpreadingDto.getUserName();
+        return ResponseEntity.ok().body(true);
     }
 
     @PostMapping("/pick-cards")
-    public Long addPickCards(@RequestBody SelectedCardsDto selectedCardsDto) throws Exception {
+    public ResponseEntity<Long> addPickCards(@RequestBody SelectedCardsDto selectedCardsDto) throws Exception {
         Reservation data = reservationRepository.findById(selectedCardsDto.getReservationIdx()).orElseThrow(() -> new Exception("예약이 존재하지 않습니다."));
 
         ListOperations<String, Object> listOperations = redisTemplate.opsForList();
@@ -73,16 +71,16 @@ public class FortuneTellingController {
         data.setSetcardsAt(LocalDateTime.now());
         reservationRepository.save(data);
 
-        return data.getIdx();
+        return ResponseEntity.ok().body(data.getIdx());
     }
 
     @GetMapping("/show-cards/{idx}")
-    public List<Object> showPickCards(@PathVariable Long idx) throws Exception {
+    public ResponseEntity<List<Object>> showPickCards(@PathVariable Long idx) throws Exception {
         ListOperations<String, Object> listOperations = redisTemplate.opsForList();
         Long size = listOperations.size(idx.toString());
         if (size.equals(0L)) throw new Exception("선택 된 카드가 없습니다.");
         List<Object> cards = listOperations.range(idx.toString(), 0, -1);
 
-        return cards;
+        return ResponseEntity.ok().body(cards);
     }
 }
